a5bd4e615ce3b4575f73c1ec7a20776b3bfa4a9b
Mr.Elijah <beztalannyj.ilia@gmail.com>
2024-11-14 11:55:11 -0800

    js-hw-three

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,15 @@
-function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding);
-  const borderWidth = parseFloat(border);
-
-  const elementWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-
-  return elementWidth;
+function filterArray(numbers, value) {
+  const numbersAbove = [];
+  for (const number of numbers) {
+    if (number > value) {
+      numbersAbove.push(number);
+    }
+  }
+  return numbersAbove;
 }
 
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
